47ae588770851ed48b8890d3e26a1d2fe02de6eb
florinalascu <florina.lascu@yahoo.com>
2023-12-20 14:20:15 +0200

    Initial Commit

---
+++ Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Florina Lascu on 20.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Florina Lascu on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
